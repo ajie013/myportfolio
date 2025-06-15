2b23a776974d6c72a62f78f563229b00819b49b0
Admin <Admin@DESKTOP-HU0I3V2>
2025-06-15 12:54:06 +0800

    optimize

--- src/app/projects/ProjectPage.tsx
+++ src/app/projects/ProjectPage.tsx
@@ -2,7 +2,7 @@
 export default function ProjectPage() {
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
-            <h1 className="text-6xl font-extrabold tracking-widest">Coming Soon</h1>
+            <h1 className="text-4xl md:text-6xl font-extrabold tracking-widest">Coming Soon</h1>
         </div>
     );
 }
